5bfa392d0d422c852e847dc42ab93b9f53347286
Serhii <writetosergiiu@gmail.com>
2023-06-27 13:31:50 +0300

    first commit

---
+++ insertionsort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "constants.h"
+
+void insertionSort(int array[], int size) {
+  for (int step = 1; step < size; step++) {
+    int key = array[step];
+    int j = step - 1;
+
+    // Compare key with each element on the left of it until an element smaller than
+    // it is found.
+    // For descending order, change key<array[j] to key>array[j].
+    while (key < array[j] && j >= 0) {
+      array[j + 1] = array[j];
+      --j;
+    }
+    array[j + 1] = key;
+  }
+}
---
+++ printarray.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "constants.h"
+
+using namespace std;
+
+// Function to print an array
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; i++) {
+    cout << array[i] << " ";
+  }
+  cout << endl;
+}
